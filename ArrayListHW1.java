83f34446d5ca9b1ce360ad5ab60ddc5772f44be6
kkim-2 <kimkailyn247@gmail.com>
2026-02-09 22:24:54 -0500

    sadfasd

--- ArrayListHW1.java
+++ ArrayListHW1.java
@@ -11,12 +11,7 @@ Call each of the methods in the main method and print out the results.  Be sure 
 import java.util.*;
 
 public class ArrayListHW1 {
-   public static void main(String[] args) {
-    ArrayList<Integer>ints = new ArrayList<>();
-    for(int i = 0; i<50; i++){
-        ints.add((int)Math.random()*50);
-    }
-   }
+
   public static int Sums(ArrayList<Integer>arr){
     int sums = 0;    
     for(int i:arr){
@@ -25,7 +20,47 @@ public class ArrayListHW1 {
 
         return sums;
    }
-    
+  public static double Averages(ArrayList<Integer>arr){
+    int sums = 0;   
+    double averages = 0; 
+    for(int i:arr){
+            sums+= i;
+        }
+    averages = (double)(sums)/arr.size();
+    return averages;
+        
+   }
+  
+  public static int Max(ArrayList<Integer>arr){
+    int max = 0;    
+    for(int i:arr){
+            if(i>max){
+              max = i;
+            }
+        }
 
+        return max;
+   }
+     public static int Min(ArrayList<Integer>arr){
+    int min = arr.get(0);    
+    for(int i:arr){
+            if(i>min){
+              min = i;
+            }
+        }
+
+        return min;
+   }
+      public static void main(String[] args) {
+    ArrayList<Integer>ints = new ArrayList<>();
+    for(int i = 0; i<50; i++){
+        ints.add((int)Math.random()*50);
+    }
+
+    System.out.println(Sums(ints));
+    System.out.println(Averages(ints));
+    System.out.println(Max(ints));
+    System.out.println(Min(ints));
+   }
 
 }
